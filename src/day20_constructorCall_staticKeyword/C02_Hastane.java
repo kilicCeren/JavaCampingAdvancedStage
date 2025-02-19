04a064d507e23ecb16d58b11166015d24201e525
kilicCeren <kiliccrn07@gmail.com>
2025-02-19 23:02:11 +0300

    v13

---
+++ src/day20_constructorCall_staticKeyword/C02_Hastane.java
@@ -0,0 +1,13 @@
+package day20_constructorCall_staticKeyword;
+
+public class C02_Hastane {
+
+    static String hastaneIsmi = "Yildiz hastanesi";
+    static String hastaneAdresi = "Cankaya";
+    static String bashekim = "Dr Esra";
+
+    String perIsim = "isim atanmamis";
+    String perAdres = "adres atanmamis";
+    String perTel = "tel atanmamis";
+
+}
